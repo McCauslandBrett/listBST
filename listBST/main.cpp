cc457acf29718a166e3d2bc4918d11f021c94e20
brettmccausland <brettmccausland@yahoo.com>
2017-09-23 14:03:38 -0700

    Notes and testing

--- listBST/main.cpp
+++ listBST/main.cpp
@@ -4,11 +4,25 @@ using namespace std;
 void nodeDriver();
 int main()
 {
-    cout << "Hello World!" << endl;
+    system("clear");
     nodeDriver();
+    cout<<endl;
     return 0;
 }
 void nodeDriver()
 {
+     cout << "nodeDriver" << endl;
     node<int> r;
+    node<int> l(3,2);
+    cout<<r;
+    r=l;
+    cout<<l;
+
+
 }
+//NOTES
+//this pointer is an implicit parameter to all member functions.
+//Therefore, inside a member function,
+//this may be used to refer to the invoking object.
+//Friend functions do not have a this pointer, because friends are not members of a class.
+//Only member functions have a this pointer.
